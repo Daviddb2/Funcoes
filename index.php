fa43fd9fe52f826a84169e1225f8b354b7f1d9b3
Davi Dias <davi22552@gmail.com>
2024-04-29 10:05:00 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+function potencia($numero, $elevado) {
+
+    $resultado = $numero;
+    for($i = 1; $i < $elevado; $i++) { 
+        $resultado = $resultado * $numero;
+    }
+    return $resultado;
+
+} 
+
+echo potencia(2, 7);
